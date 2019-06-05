cb611646ae03e8c59640ab16c029d8d22645805c
hitmachut <hitmachut@students.local>
2019-06-05 18:51:23 +0300

    05/06 add product connect to the server

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -7,6 +7,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { ParameterOfProduct } from 'src/app/models/parameter-of-product';
 import { ParameterService } from '../../services/parameter.service';
 import { Parameter } from 'src/app/models/parameter';
+import { ProductService } from '../../services/product.service';
 
 
 @Component({
@@ -18,65 +19,75 @@ export class ProductComponent {
   product: Product = new Product();
   categories: Category[] = [];
   parametersAreExist: Parameter[] = [];
-  ParameterOfProductAreExist: ParameterOfProduct[]=[];
-
+  ParameterOfProductAreExist: ParameterOfProduct[] = [];
+  mainCategoryID: number;
   NewParameters: Parameter[] = [];
-  NewParameterOfProduct: ParameterOfProduct[]=[];
+  NewParameterOfProduct: ParameterOfProduct[] = [];
 
-  constructor(private CategoryService: CategoryService, private ParameterService: ParameterService) { }
+  constructor(private CategoryService: CategoryService, private ParameterService: ParameterService
+    , private ProductService: ProductService) { }
 
   addParameter() {
     this.NewParameters.push(new Parameter());
-    this.NewParameterOfProduct.push(new ParameterOfProduct);
+    this.NewParameterOfProduct.push(new ParameterOfProduct());
   }
 
   ngOnInit() {
 
+    this.NewParameterOfProduct.push(new ParameterOfProduct());
     this.NewParameters.push(new Parameter());
-    this.NewParameterOfProduct.push(new ParameterOfProduct);
-
+    this.product.CleverAgent = false;
     this.CategoryService.getCategories().subscribe((res: Category[]) => {
       if (res != null) {
-     
+
         this.categories = res;
-         if(this.categories!=null)
-         this.product.CategoryId=this.categories[0].CategoryId;
-         this.onCategoryChanged();
+        if (this.categories != null)
+          this.mainCategoryID = this.categories[0].CategoryId;
+        this.onCategoryChanged(true);
 
       }
 
     }, (err: HttpErrorResponse) => {
     });
-     
+
   }
 
-  
-  onCategoryChanged() {
 
-    this.ParameterService.getParametersOfCategory(this.product.CategoryId).subscribe((res: Parameter[])=> {
+  onCategoryChanged(isMain: boolean) {
+
+    let categoryID: number;
+    if (isMain) {
+      categoryID = this.mainCategoryID;
+      this.product.CategoryId = -1;
+    } else {
+      categoryID = this.product.CategoryId;
+    }
+
+    this.ParameterService.getParametersOfCategory(categoryID).subscribe((res: Parameter[]) => {
       if (res != null) {
         this.parametersAreExist = res;
-        this.ParameterOfProductAreExist.length=this.parametersAreExist.length;
-        
-        
-        
+        this.parametersAreExist.forEach(item => {
+          let newParameter = new ParameterOfProduct();
+          this.ParameterOfProductAreExist.push(newParameter);
+        });
 
       }
 
-    }, (err: HttpErrorResponse) => {    });
+    }, (err: HttpErrorResponse) => { });
 
   }
 
   OnAddProduct(myForm: NgForm) {
     // עדכון קוד הפרמטר בטבלת פרמטרים למוצר
-    for(let i=0;i<this.parametersAreExist.length;i++)
-        {
-          this.ParameterOfProductAreExist[i].ParameterId=this.parametersAreExist[i].ParameterId;
-        }
-        // עדכון הקטגוריה בכל פרמטרים החדשים
-    for(var i=0;i<this.NewParameters.length;i++)
-    {
-      this.NewParameters[i].CategoryId=this.product.CategoryId;
+    for (let i = 0; i < this.parametersAreExist.length; i++) {
+      this.ParameterOfProductAreExist[i].ParameterId = this.parametersAreExist[i].ParameterId;
     }
+    // עדכון הקטגוריה בכל פרמטרים החדשים
+    
+    for (var i = 0; i < this.NewParameters.length; i++) {
+      this.NewParameters[i].CategoryId = this.product.CategoryId?this.product.CategoryId:this.mainCategoryID;
+    }
+    this.ProductService.AddProduct(this.product,this.ParameterOfProductAreExist,
+     [],[]).subscribe();
   }
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Product} from '../models/product';
+import {Parameter} from '../models/parameter';
+import {ParameterOfProduct} from '../models/parameter-of-product';
+
+const url = "http://localhost:65051/";
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+   AddProduct(product:Product,ParameterOfProductAreExist:ParameterOfProduct[]
+    , NewParameters:any,parametersAreExist:Parameter[]) {
+
+      let dynamicObj = {
+        prod: product,
+        ParameterOf: ParameterOfProductAreExist,
+        NewParameters: NewParameters,
+        parametersAreExist: parametersAreExist
+      }
+    //subscribe דוגמא לשליחת הרבה אוביקטים ע"י שליחה להרבה פונקציות בקונטרולר ועד שלא כל הפונקציות מחזירות תשובה לא יאותחל ה
+   /*    let product_request = this.http.post(url + "api/product/AddProduct", product ) ;
+      let ParameterOfProductAreExist_request = this.http.post(url + "api/product/AddProduct", ParameterOfProductAreExist ) ;
+  
+      forkJoin([product_request, ParameterOfProductAreExist_request]).subscribe(results => {
+        // results[0] is our product_request
+        // results[1] is our ParameterOfProductAreExist_request */
+
+     return this.http.post(url + "api/product/AddProduct", dynamicObj )  ;
+  }
+}
